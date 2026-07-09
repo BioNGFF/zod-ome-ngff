3c51e90dac6a8f6820155f1791989650b62ec65a
Alex Surtees <ats21994@gmail.comn>
2026-07-09 15:51:27 +0100

    Remove array from XMLAnnotation schema definition

--- src/bioformats2raw.ts
+++ src/bioformats2raw.ts
@@ -1207,7 +1207,7 @@ const structuredAnnotationsTypeSchema = baseTypeSchema.extend({
   TagAnnotation: z.array(tagAnnotationTypeSchema).optional(),
   TermAnnotation: z.array(termAnnotationTypeSchema).optional(),
   TimestampAnnotation: z.array(timestampAnnotationTypeSchema).optional(),
-  XMLAnnotation: z.array(xMLAnnotationTypeSchema).optional(),
+  XMLAnnotation: xMLAnnotationTypeSchema.optional(),
 });
 
 const shapeGroupProxyTypeSchema = baseTypeSchema.extend({
